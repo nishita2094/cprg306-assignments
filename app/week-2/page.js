2bcc749d9d6dd919fff0839f32c559a07193ecf0
nishita2094 <nishita.grover@edu.sait.ca>
2025-05-13 13:49:21 -0600

    final

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,6 +3,7 @@ import Studentinfo from "./student-info";
 export default function Page() {
     return (
         <main>
+            <h1>Shopping List</h1>
             <Studentinfo />
         </main>
   );
