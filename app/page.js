233c8d7ad03afa7a9700757723099a14a0dd92c8
nishita2094 <nishita.grover@edu.sait.ca>
2025-05-13 13:47:28 -0600

    Final Commit

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Shopping List</h1>
+      <h1>CPRG 306: Web Development 2</h1>
       <Link href="/week-2" className="text-2xl text-cyan-600 hover:text-cyan-400">Week 2</Link>
   </main>
 );
